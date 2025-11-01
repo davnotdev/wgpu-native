2676d3bc30e350b218f3518552ce12fa7bc36427
davnotdev <usr21p1@gmail.com>
2025-10-31 17:19:41 -0700

    Patch in new wgpu 26 limits that 25 cannot reach

--- src/conv.rs
+++ src/conv.rs
@@ -484,6 +484,9 @@ pub fn map_required_limits(
     extras: Option<&native::WGPUNativeLimits>,
 ) -> wgt::Limits {
     let mut wgt_limits = base_limits;
+    wgt_limits.max_binding_array_elements_per_shader_stage = 999;
+    wgt_limits.max_binding_array_sampler_elements_per_shader_stage = 999;
+
     if limits.maxTextureDimension1D != native::WGPU_LIMIT_U32_UNDEFINED {
         wgt_limits.max_texture_dimension_1d = limits.maxTextureDimension1D;
     }
